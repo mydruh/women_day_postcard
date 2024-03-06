587cbcfaacb9736f06cd33e0ee76f1464b9d0583
Sultan Yesbolatov <yesbolatov.s@applecity.kz>
2024-03-06 18:43:44 +0500

    fix

--- js/app.js
+++ js/app.js
@@ -122,7 +122,7 @@ const main = async () => {
         "Поздравляем тебя с Международным Женским Днем! Пусть этот день станет началом новых достижений и свершений. Желаем тебе быть счастливой и уверенной в себе каждый день!",
     },
   }
-  const congrat = congrats['kuanysheva.a@appleity.kz']
+  const congrat = congrats[userInfo.email] || congrats.default;
   title_block.innerText = congrat.title;
   var with_love = congrat?.message
 

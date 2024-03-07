95fc4e5d1d3de72c0cc1383735a5ba09cab8ae51
Sultan Yesbolatov <yesbolatov.s@applecity.kz>
2024-03-07 10:23:43 +0500

    add music page

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
-    base: "/women_day_postcard/"
-}
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+
+export default defineConfig({
+    base: "/women_day_postcard/",
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            music: resolve(__dirname, 'pages/music.html'),
+          },
+        },
+      },
+})
